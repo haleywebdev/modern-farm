3d2593635667f744007e7eb4469fc7aee4ccfe2c
Haley Wilson <haleyjwilson20@gmail.com>
2021-10-18 09:03:34 -0500

    Commit

---
+++ src/scripts/seeds/asparagus.js
@@ -0,0 +1,8 @@
+export const createAsparagus = (type, height, output) => {
+
+    return createAsparagus
+}
+
+const returnAsparagus = createAsparagus(Asparagus, 24, 4)
+
+console.log(returnAsparagus)
